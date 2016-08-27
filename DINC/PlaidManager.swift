a8068aaaf630e35e4172a90a183b209aa4db35e0
danielhour <daniel.hour@me.com>
2016-08-27 11:10:11 -0700

    this is a watch specific app so commenting out all iOS code

--- DINC/PlaidManager.swift
+++ DINC/PlaidManager.swift
@@ -6,39 +6,39 @@
 //  Copyright © 2016 DHour. All rights reserved.
 //
 
-import Foundation
-import SwiftyJSON
-
-
-/**
- *
- */
-class PlaidManager {
-    
-    
-    /**
-     Makes the API Request to fetch a user's transactions
-     
-     - parameter request:    the `FetchTransactions` request
-     - parameter completion: completion handler that sends back an array of `Transaction`s
-     */
-    static func fetchTransactions(request: PlaidService.Router, completion: ([Transaction]?)->()) {
-        let plaidService = PlaidService()
-        
-        plaidService.makeAPIRequest(request) { (error, json) in
-            guard error == nil else {
-                magic(error!.localizedDescription)
-                completion(nil)
-                return
-            }
-            guard let dataArray = json?["transactions"].arrayObject as? [[String: AnyObject]] else {
-                magic(JsonError.Empty)
-                completion(nil)
-                return
-            }
-            
-            let transactions = dataArray.map{Transaction(transaction: $0)}
-            completion(transactions)
-        }
-    }
-}
+//import Foundation
+//import SwiftyJSON
+//
+//
+///**
+// *
+// */
+//class PlaidManager {
+//    
+//    
+//    /**
+//     Makes the API Request to fetch a user's transactions
+//     
+//     - parameter request:    the `FetchTransactions` request
+//     - parameter completion: completion handler that sends back an array of `Transaction`s
+//     */
+//    static func fetchTransactions(request: PlaidService.Router, completion: ([Transaction]?)->()) {
+//        let plaidService = PlaidService()
+//        
+//        plaidService.makeAPIRequest(request) { (error, json) in
+//            guard error == nil else {
+//                magic(error!.localizedDescription)
+//                completion(nil)
+//                return
+//            }
+//            guard let dataArray = json?["transactions"].arrayObject as? [[String: AnyObject]] else {
+//                magic(JsonError.Empty)
+//                completion(nil)
+//                return
+//            }
+//            
+//            let transactions = dataArray.map{Transaction(transaction: $0)}
+//            completion(transactions)
+//        }
+//    }
+//}
